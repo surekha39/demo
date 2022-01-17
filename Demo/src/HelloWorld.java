495bf5b039698b8d025b540136f6c660222f2694
potusurekhampha <potusurekhampha@ip-172-31-85-10.ec2.internal>
2022-01-17 06:50:15 +0000

    helloworld

---
+++ Demo/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+  //shortcut for main is type main + ctrl spacebar and enter
+
+  public static void main(String[] args) {
+ 	 //syso+ctrl+space bar and enter
+ 	 System.out.println("Hello world");
+  }
+}
